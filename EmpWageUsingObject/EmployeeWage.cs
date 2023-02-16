361e7b4ded102523a387703a0c4d58d68ba6802c
Your Name <you@example.com>
2023-02-16 22:19:08 +0530

    Checking employee is present or absent

---
+++ EmpWageUsingObject/EmployeeWage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageUsingObject
+{
+    public class EmployeeWage
+    {
+        public void CheckEmployee()
+        {
+            const int IS_PRESENT = 1;
+            Random random = new Random();
+            int employeeCheck = random.Next(2);
+
+            if (employeeCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
--- EmpWageUsingObject/Program.cs
+++ EmpWageUsingObject/Program.cs
@@ -8,6 +8,9 @@ namespace EmpWageUsingObject
         {
             Console.WriteLine("Welcome to employee wage computation program");
 
+            EmployeeWage obj = new EmployeeWage();
+            obj.CheckEmployee();
+
             Console.ReadLine();
         }
     }
