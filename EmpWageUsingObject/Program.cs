77b78bf7f254cf2fa544069ac18e4ee4ee046d20
Your Name <you@example.com>
2023-02-20 16:42:11 +0530

    calculating employee wage for multiple companies

--- EmpWageUsingObject/Program.cs
+++ EmpWageUsingObject/Program.cs
@@ -9,7 +9,8 @@ namespace EmpWageUsingObject
             Console.WriteLine("Welcome to employee wage computation program");
 
             EmployeeWage obj = new EmployeeWage();
-            obj.CheckEmployee();
+            obj.ComputeEmpWage("DMart", 20, 20, 100);
+            obj.ComputeEmpWage("Reliance", 10, 15, 70);
 
             Console.ReadLine();
         }
